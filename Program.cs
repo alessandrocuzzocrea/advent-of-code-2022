c8ce598e68f6a2f8980cf940482ce39fbd315e8b
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2022-12-01 19:13:34 +0900

    Day 1: Calorie Counting

---
+++ Program.cs
@@ -0,0 +1,6 @@
+Console.WriteLine("🎄Advent of Code 2022🎄");
+
+Day1.Part1();
+Day1.Part2();
+
+Console.WriteLine("Have a good one! 👋");
---
+++ solutions/Day1.cs
@@ -0,0 +1,53 @@
+
+public class Day1
+{
+    const string InputFilePath = "./solutions/inputs/day1";
+
+    public static void Part1()
+    {
+        int maxCalories = int.MinValue;
+        int currentCalories = 0;
+
+        var lines = File.ReadAllLines(InputFilePath).ToList();
+        lines.Add("");
+        foreach (string line in lines)
+        {
+            if (line == "")
+            {
+                maxCalories = int.Max(maxCalories, currentCalories);
+                currentCalories = 0;
+            }
+            else
+            {
+                currentCalories += int.Parse(line);
+            }
+        }
+
+        Console.WriteLine(maxCalories);
+    }
+
+    public static void Part2()
+    {
+        int currentCalories = 0;
+        List<int> elves = new List<int>();
+
+        var lines = File.ReadAllLines(InputFilePath).ToList();
+        lines.Add("");
+        foreach (string line in lines)
+        {
+            if (line == "")
+            {
+                elves.Add(currentCalories);
+                currentCalories = 0;
+            }
+            else
+            {
+                currentCalories += int.Parse(line);
+            }
+        }
+
+        elves.Sort();
+        elves.Reverse();
+        Console.WriteLine(elves[0] + elves[1] + elves[2]);
+    }
+}
