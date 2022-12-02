31dd2107d91d3755c982833072b930ba4ea0c712
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2022-12-03 08:22:31 +0900

    Add xUnit

---
+++ AOC2022.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+
+
+namespace AOC2022.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        throw new NotImplementedException("Not fully implemented.");
+    }
+}
