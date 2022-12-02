e1296b884f9b544316900cbbb11585879a4fc0d9
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2022-12-02 23:23:00 +0900

    Refactor Day 2

--- solutions/Day2.cs
+++ solutions/Day2.cs
@@ -1,35 +1,15 @@
+using System.Diagnostics;
+
 class Day2
 {
-
     const string InputFilePath = "./solutions/inputs/day2";
+
+    static readonly string[] Win = new string[3] { "A Y", "B Z", "C X" };
+    static readonly string[] Draw = new string[3] { "A X", "B Y", "C Z" };
+    static readonly string[] Lose = new string[3] { "A Z", "B X", "C Y" };
 
     public static void Part1()
     {
-
-        //Rock    -> A, X
-        //Paper   -> B, Y
-        //Scissor -> C, Z
-
-        // WIN
-        // A Y
-        // B Z
-        // C X
-
-        string[] win = new string[3] { "A Y", "B Z", "C X" };
-
-        // Draw
-        // A X
-        // B Y
-        // C Z
-
-        string[] draw = new string[3] { "A X", "B Y", "C Z" };
-
-        // Lose
-        // A Z
-        // B X
-        // C Y
-
-        string[] lose = new string[3] { "A Z", "B X", "C Y" };
 
         var fightPoints = new Dictionary<string, int> {
             {"A X", 3}
@@ -50,27 +30,23 @@ class Day2
             var opponent = roundSplit[0];
             var you = roundSplit[1];
 
-            if (win.Contains(round))
+            if (Day2.Win.Contains(round))
             {
                 score += 6;
             }
-            else if (draw.Contains(round))
+            else if (Day2.Draw.Contains(round))
             {
                 score += 3;
             }
             score += points[you];
         }
 
+        Debug.Assert(score == 11873);
         Console.WriteLine(score);
-
     }
 
     public static void Part2()
     {
-        string[] win = new string[3] { "A Y", "B Z", "C X" };
-        string[] draw = new string[3] { "A X", "B Y", "C Z" };
-        string[] lose = new string[3] { "A Z", "B X", "C Y" };
-
         var points = new Dictionary<string, int> {
             {"X", 1},
             {"Y", 2},
@@ -78,8 +54,7 @@ class Day2
         };
 
         var score = 0;
-
-        string[] a; 
+        string[] a;
 
         var rounds = File.ReadLines(InputFilePath);
         foreach (var round in rounds)
@@ -87,23 +62,21 @@ class Day2
             var roundSplit = round.Split(' ');
             var opponent = roundSplit[0];
             var you = roundSplit[1];
-            // var you = "";
 
-            if (you == "X") // Lose
+            if (you == "X")
             {
-                a = lose;
+                a = Day2.Lose;
                 score += 0;
             }
             else if (you == "Y")
             {
-                a = draw;
+                a = Day2.Draw;
                 score += 3;
             }
             else
-            { // WIN
-                a = win;
+            {
+                a = Day2.Win;
                 score += 6;
-
             }
 
             foreach (var combo in a)
@@ -116,6 +89,8 @@ class Day2
 
             score += points[you];
         }
+
+        Debug.Assert(score == 12014);
         Console.WriteLine(score);
     }
 }
