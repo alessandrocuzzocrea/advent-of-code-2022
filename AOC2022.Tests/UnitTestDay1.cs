730e92cdef7b67a8c9815d6909f93830a711f016
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2022-12-06 00:15:26 +0900

    Add Day 1 Part 2 test

--- AOC2022.Tests/UnitTestDay1.cs
+++ AOC2022.Tests/UnitTestDay1.cs
@@ -9,4 +9,10 @@ public class UnitTestDay1
     {
         Assert.Equal(24000, Day1.Part1("./inputs/day1-example"));
     }
+
+    [Fact]
+    public void Part2_IsCorrect_WhenUsingExampleInputFile()
+    {
+        Assert.Equal(45000, Day1.Part2("./inputs/day1-example"));
+    }
 }
