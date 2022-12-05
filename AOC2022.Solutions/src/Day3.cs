69d1fcc119448c6f227ced9aa3d0fe124a7d0834
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2022-12-06 02:10:22 +0900

    Refactor Day 4

--- AOC2022.Solutions/src/Day3.cs
+++ AOC2022.Solutions/src/Day3.cs
@@ -2,66 +2,67 @@ public class Day3
 {
     public static int Part1(string inputFilePath)
     {
-        var prioritySum = 0;
+        var prioritiesSum = 0;
 
         var rucksacks = File.ReadLines(inputFilePath);
         foreach (var rucksack in rucksacks)
         {
-            var comp1 = rucksack.Substring(0, (int)rucksack.Length / 2).ToCharArray();
-            var comp2 = rucksack.Substring((int)rucksack.Length / 2, (int)rucksack.Length / 2).ToCharArray();
+            var compartments = SplitStringInHalf(rucksack);
+            var set1 = new HashSet<char>(compartments.Item1);
+            var set2 = new HashSet<char>(compartments.Item2);
 
-            var comp1Set = new HashSet<char>(comp1);
-            var comp2Set = new HashSet<char>(comp2);
-
-            var overlap = comp1Set.Intersect(comp2Set);
+            var overlap = set1.Intersect(set2);
 
             foreach (var tool in overlap)
             {
-                var ascii = (int) tool;
-                if (Char.IsLower(tool)) {
-                    ascii -= 96;
-                } else {
-                    ascii -= (64 - 26);
-                }
-
-                // Console.WriteLine($"{tool}: {ascii}");
-                prioritySum += ascii;
+                prioritiesSum += CalcPriority(tool);
             }
         }
 
-        return prioritySum;
+        return prioritiesSum;
     }
 
-    public static int Part2(string inputFilePath) {
-        var prioritySum = 0;
+    public static int Part2(string inputFilePath)
+    {
+        var prioritiesSum = 0;
 
         var rucksacks = File.ReadLines(inputFilePath).ToArray();
         for (var i = 0; i < rucksacks.Length; i += 3)
         {
-            var ruck1 = rucksacks[i];
-            var ruck2 = rucksacks[i+1];
-            var ruck3 = rucksacks[i+2];
+            var set1 = new HashSet<char>(rucksacks[i]);
+            var set2 = new HashSet<char>(rucksacks[i + 1]);
+            var set3 = new HashSet<char>(rucksacks[i + 2]);
 
-            var ruck1Set = new HashSet<char>(ruck1);
-            var ruck2Set = new HashSet<char>(ruck2);
-            var ruck3Set = new HashSet<char>(ruck3);
-
-            var overlap = ruck1Set.Intersect(ruck2Set).Intersect(ruck3Set);
+            var overlap = set1.Intersect(set2).Intersect(set3);
 
             foreach (var tool in overlap)
             {
-                var ascii = (int) tool;
-                if (Char.IsLower(tool)) {
-                    ascii -= 96;
-                } else {
-                    ascii -= (64 - 26);
-                }
-
-                // Console.WriteLine($"{tool}: {ascii}");
-                prioritySum += ascii;
+                prioritiesSum += CalcPriority(tool);
             }
         }
 
-        return prioritySum;
+        return prioritiesSum;
+    }
+
+    static (char[], char[]) SplitStringInHalf(string s)
+    {
+        var comp1 = s.Substring(0, (int)s.Length / 2).ToCharArray();
+        var comp2 = s.Substring((int)s.Length / 2, (int)s.Length / 2).ToCharArray();
+
+        return (comp1, comp2);
+    }
+
+    static int CalcPriority(char tool)
+    {
+        var ascii = (int)tool;
+        if (Char.IsLower(tool))
+        {
+            ascii -= 96;
+        }
+        else
+        {
+            ascii -= (64 - 26);
+        }
+        return ascii;
     }
 }
